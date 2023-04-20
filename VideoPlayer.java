4c42b883109fc1b02c2cdedb89213a3e6a55f377
Antonio Monje <monje001@cougars.csusm.edu>
2023-04-20 07:49:00 -0700

    attempt to extract video data to create table of contents

--- VideoPlayer.java
+++ VideoPlayer.java
@@ -1,9 +1,12 @@
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.awt.image.BufferedImage;
+
+import javax.imageio.stream.FileImageInputStream;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -32,6 +35,13 @@ import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.event.MouseAdapter;
 import java.awt.Cursor;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import javax.imageio.stream.FileImageInputStream;
+import javax.swing.table.DefaultTableModel;
+
 
 public class VideoPlayer {
     private final static int BUFFER_SIZE = 1024 * 1024;
@@ -44,6 +54,7 @@ public class VideoPlayer {
 
     private static boolean isPaused = false;
     private static int currentFrame = 0;
+    private static byte[] data = readRGBFile("./InputVideo.rgb", currentFrame);
 
     // Add a method to jump to a specific frame
     public static void jumpToFrame(int scene, int shot, int subshot, int numFrames) {
@@ -94,6 +105,119 @@ public class VideoPlayer {
         currentFrame = sceneFrame + shotFrame + subshotFrame;
     }
 
+    public static DefaultTableModel extractVideoMetaData(String filePath, int currentFrame)
+    {
+        //read the video frames
+       byte[] data = readRGBFile(filePath, currentFrame);
+
+        //create table model to store data
+        DefaultTableModel tableModel = new DefaultTableModel();
+        //add columns
+        //tableModel.addColumn("Movie");
+        tableModel.addColumn("Scene");
+        tableModel.addColumn("Shot");
+        tableModel.addColumn("Subshot");
+
+        int currentSceneValue = 0;
+        int currentShotValue = 0;
+        int currentSubshotValue = 0;
+        
+        String scene = "";
+        String shot = "";
+        String subshot = "";
+
+        //if(tableModel.getRowCount() == 0){
+        //    tableModel.addRow(new Object[]{movie,"","",""});
+        //}
+
+        String currentScene = "";
+        String currentShot = "";
+        String currentSubshot = "";
+
+        for(int i = 0; i < data.length; i++){
+            scene = extractScene(data[i], currentSceneValue);
+            shot = extractShot(data[i], currentShotValue);
+            subshot = extractSubshot(data[i],currentSubshotValue);
+            
+            //check for new scene
+            if(!scene.equals(currentScene)){
+                tableModel.addRow(new Object[]{"Scene " + scene, "", ""});
+                currentScene = scene;
+                currentShot = "";
+                currentSubshot = "";
+            }
+
+            //check for new shot
+            if(!shot.equals(currentShot)){
+                tableModel.addRow(new Object[]{"", "shot " + shot, ""});
+                currentShot = shot;
+                currentSubshot = "";
+            }
+
+             //check for new subshot
+             if(!subshot.equals(currentSubshot)){
+                tableModel.addRow(new Object[]{"", "", "subshot " + subshot});
+                currentSubshot = subshot;
+            }
+            
+            //tableModel.addRow(new Object[]{scene, shot, subshot});
+        }
+        return tableModel;
+    }
+   
+    
+
+    private static String extractScene(byte videoData, int currentSceneValue){
+        int sceneValue = (videoData & 0xC0) >> 6;
+        currentSceneValue += sceneValue + 1;
+        return String.valueOf(currentSceneValue);
+    }
+
+    private static String extractShot(byte videoData, int currentShotValue){
+        int shotValue = (videoData & 0x38) >> 3;
+        currentShotValue += shotValue + 1;
+        return String.valueOf(currentShotValue);
+    }
+
+    private static String extractSubshot(byte videoData, int currentSubshotValue){
+        int subshotValue = videoData & 0x07;
+        currentSubshotValue += subshotValue + 1;
+        return String.valueOf(currentSubshotValue);
+    }
+
+    private static byte[] readRGBFile(String filePath, int currentFrame){
+        int width = 480;
+        int height = 270;
+        int fps = 30;
+        int numFrames = 8682;
+        File file = new File(filePath);
+        byte[] data = null;
+        try {
+            RandomAccessFile raf = new RandomAccessFile(file, "r");
+            FileChannel channel = raf.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(width * height * 3);
+            long frameStartTime = System.currentTimeMillis();
+            long frameDuration = 1000 / fps;
+           
+            //Set position based on current frame
+            if(currentFrame >=0 && currentFrame < numFrames){
+                int numBytesPerFrame = width * height * 3;
+                channel.position((long) currentFrame * numBytesPerFrame);
+            }
+            buffer.clear();
+            channel.read(buffer);
+            buffer.flip();
+            byte[] imageData = new byte[buffer.remaining()];
+            buffer.get(imageData);
+            data = imageData;
+
+
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public static void main(String[] args) {
         File file = new File("./InputVideo.rgb");
         int width = 480;
@@ -117,23 +241,36 @@ public class VideoPlayer {
 
         // Add a separate panel for the table of contents
         JPanel tablePanel = new JPanel();
-        frame.add(tablePanel, BorderLayout.EAST);
+        frame.add(tablePanel, BorderLayout.WEST);
 
+/*
         // Sample data for the table of contents
         Object[][] data = {
             {"Scene 1", "Shot 1", "Subshot 1"},
-            {"Scene 2", "Shot 1", "Subshot 1"},
-            {"Scene 3", "Shot 1", "Subshot 1"},
+            {"Scene 1", "Shot 2", ""},
+            {"Scene 1", "Shot 3", ""},
+            {"Scene 2", "Shot 1", ""},
+            {"Scene 2", "Shot 2", ""},
+            {"Scene 2", "Shot 3", "Subshot 1"},
+            {"Scene 3", "Shot 1", ""},
+            {"Scene 3", "Shot 2", "Subshot 1"},
+            {"Scene 3", "Shot 3", ""},
         };
 
         // Column names for the table
         String[] columnNames = {"Scene", "Shot", "Subshot"};
 
+
         // Create a JTable with the sample data and column names
         DefaultTableModel tableModel = new DefaultTableModel(data, columnNames);
         JTable tableOfContents = new JTable(tableModel);
+*/
 
-        
+         // Create a JTable with the sample data and column names
+         DefaultTableModel tableModel = extractVideoMetaData("./InputVideo.rgb", currentFrame);
+         JTable tableOfContents = new JTable(tableModel);
+
+
 
         tableOfContents.addMouseListener(new MouseAdapter() {
             @Override
@@ -146,9 +283,9 @@ public class VideoPlayer {
                     // Calculate the frame number for the middle of the movie
             
                     // Get the values for the clicked row
-                    int scene = Integer.parseInt(data[row][0].toString().split(" ")[1]);
-                    int shot = Integer.parseInt(data[row][1].toString().split(" ")[1]);
-                    int subshot = Integer.parseInt(data[row][2].toString().split(" ")[1]);
+                    int scene = Integer.parseInt(tableOfContents.getValueAt(row, 0).toString().split(" ")[1]);
+                    int shot = Integer.parseInt(tableOfContents.getValueAt(row, 1).toString().split(" ")[1]);
+                    int subshot = Integer.parseInt(tableOfContents.getValueAt(row, 2).toString().split(" ")[1]);
                     
                     // Pause the video
                     isPaused = true;
@@ -169,6 +306,26 @@ public class VideoPlayer {
             @Override
             public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+
+                 //calculate the identation level based on the row
+                 int indentationlevel = 0;
+                 if(row > 0) {
+                     String previousScene = tableOfContents.getValueAt(row-1, 0).toString();
+                     String currentScene = tableOfContents.getValueAt(row, 0).toString();
+                     if(!currentScene.equals(previousScene)){
+                         indentationlevel = 0;
+                     } else {
+                         indentationlevel = tableOfContents.getValueAt(row-1, column).toString().split("\\s+").length;
+                     }
+                 }
+ 
+                 //add indentation based on the indentation level
+                 String indentation = "";
+                 for(int i = 0; i < indentationlevel; i++){
+                     indentation += "    "; // 1 tab for each level
+                 }
+                 setValue(indentation + value.toString());
+
                 if (column == 0) {
                     c.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
                 } else {
@@ -182,8 +339,14 @@ public class VideoPlayer {
         JScrollPane tableScrollPane = new JScrollPane(tableOfContents);
         tableScrollPane.setPreferredSize(new Dimension(300, 150));
 
+
         // Add the JScrollPane to the table panel
-        tablePanel.add(tableScrollPane);
+        tablePanel.add(tableScrollPane, BorderLayout.WEST);
+        //add the table to the frame
+        frame.add(tableScrollPane, BorderLayout.WEST);
+        frame.pack();
+        frame.setVisible(true);
+        
 
 
         JButton playButton = new JButton("Play");
